6ecc56900e10fdc87729cfd37cba2f39a9b9eeb4
Sayan Roychowdhury <sayan.royc@gmail.com>
2016-03-15 09:47:03 -0500

    Deleted delivery address update function
    update works exactly like create_new, just run create_new when we want
    to update an address (same Datastore costs, less code)

--- appengine-flask-skeleton-master/delivery_address.py
+++ appengine-flask-skeleton-master/delivery_address.py
@@ -7,6 +7,7 @@ from error_handlers import InvalidUsage
 app = Flask(__name__)
 
 # Create a new delivery_address object and put into Datastore
+# Update_delivery_address should be done using the same function
 @app.route('/delivery_address/create/user_id=<user_id>', methods=['POST'])
 def create_delivery_address(user_id):
 	json_data 		= request.get_json()
@@ -30,76 +31,32 @@ def create_delivery_address(user_id):
 	geo_point = ndb.GeoPt(location.latitude,location.longitude)
 
 
-	a = Delivery_Address(address_line_1=address_line_1, address_line_1=address_line_1,
+	a = Delivery_Address(address_line_1=address_line_1, address_line_1=address_line_1, 
 						 city=city, state=state, country=country, zip_code=zip_code, 
 						 geo_point=geo_point)
 
 	# Wrap in try/except block?
-	delivery_address_key = a.put()
-	u.home_address = delivery_address_key
+	u.home_address = a
 	u.put()
 
-	# return "Delivery address successfully created." 201
-	data = {'delivery_address_id':delivery_address_key.id()}
-	resp = jsonify(data)
-	resp.status_code = 201
-	return resp
+	return "User home address successfully created.", 201
 
 
 
 
 # Delete a delivery address from Datastore
-@app.route('/delivery_address/delete/delivery_address_id=<int:delivery_address_id>', methods=['DELETE'])
-def delete_delivery_address(delivery_address_id):
-	ndb.Key('Delivery_Address', delivery_address_id).delete()
+@app.route('/delivery_address/delete/user_id=<user_id>', methods=['DELETE'])
+def delete_delivery_address(user_id):
+	u = User.get_by_id(user_id)
+	if u is None:
+		raise InvalidUsage('UserID does not match any existing user', status_code=400)
 
-	data = {'delivery_address_id deleted':delivery_address_id}
-	resp = jsonify(data)
-	resp.status_code = 200
-	return resp
+	u.home_address = None
+	u.put()
+
+	return "User home address successfully deleted.", 200
 
 
-
-
-# Update a delivery address
-@app.route('/delivery_address/update/delivery_address_id=<int:delivery_address_id>', methods=['POST'])
-def update_delivery_address(delivery_address_id):
-	json_data 		= request.get_json()
-	address_line_1 	= json_data.get('address_line_1','')
-	address_line_2 	= json_data.get('address_line_2','')
-	city 			= json_data.get('city','')
-	state 			= json_data.get('state','')
-	zip_code 		= json_data.get('zip_code','')
-	country			= json_data.get('country','')
-
-	# Get the delivery address
-	a = Delivery_Address.get_by_id(delivery_address_id)
-	if a is None:
-		raise InvalidUsage('Delivery Address ID does not match any existing Delivery Address.', status_code=400)
-
-	# Get latitude/longitude info
-	address_info = [address_line_1, address_line_2, city, state, zip_code, country]
-	geolocator = Nominatim()
-	location = geolocator.geocode(" ".join(address_info))
-	geo_point = ndb.GeoPt(location.latitude,location.longitude)
-
-	# Update the attributes
-	a.json_data = json_data
-	a.address_line_1 = address_line_1
-	a.address_line_2 = address_line_2
-	a.city = city
-	a.state = state
-	a.zip_code = zip_code
-	a.country = country
-	a.geo_point = geo_point
-
-	delivery_address_key = a.put()
-
-	# Return the attributes of the new item
-	data = {'delivery_address_id':delivery_address_key.id()}
-	resp = jsonify(data)
-	resp.status_code = 200
-	return resp
 
 
 ### Server Error Handlers ###
